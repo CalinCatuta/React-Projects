cbeae60a2de3742b8da9fa3600d2358e3bd56328
CalinCatuta <calinut123@yahoo.co.uk>
2023-10-31 15:14:06 +0200

    add logout

--- house-finder/src/pages/Profile.jsx
+++ house-finder/src/pages/Profile.jsx
@@ -1,13 +1,32 @@
 import { getAuth } from "firebase/auth";
-import { useState, useEffect } from "react";
+import { useState } from "react";
+import { useNavigate, Link } from "react-router-dom";
+
 const Profile = () => {
-  const [user, setUser] = useState(null);
   const auth = getAuth();
-  useEffect(() => {
-    setUser(auth.currentUser);
-    console.log(auth.currentUser);
-  }, []);
-  return user ? <h1>{user.displayName}</h1> : "not logged";
+  const [formData, setFormData] = useState({
+    name: auth.currentUser.displayName,
+    email: auth.currentUser.email,
+  });
+  const { name, email } = formData;
+
+  const navigate = useNavigate();
+
+  const onLogoutHan = (e) => {
+    auth.signOut();
+    navigate("/");
+  };
+
+  return (
+    <div className="profile">
+      <header className="profileHeader">
+        <p className="pageHeader">My Profile</p>
+        <button type="button" className="logOut" onClick={onLogoutHan}>
+          Logout
+        </button>
+      </header>
+    </div>
+  );
 };
 
 export default Profile;
