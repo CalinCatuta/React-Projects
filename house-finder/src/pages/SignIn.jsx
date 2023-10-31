0493e44dbc0dabbdec5e0d3ba57c40bdffbed205
CalinCatuta <calinut123@yahoo.co.uk>
2023-10-31 18:00:31 +0200

    sign in with google

--- house-finder/src/pages/SignIn.jsx
+++ house-finder/src/pages/SignIn.jsx
@@ -7,6 +7,7 @@ import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 
 import { ReactComponent as ArrowRightIcon } from "../assets/svg/keyboardArrowRightIcon.svg";
 import visibilityIcon from "../assets/svg/visibilityIcon.svg";
+import OAuth from "../components/OAuth";
 
 function SignIn() {
   const [showPass, setShowPass] = useState(false);
@@ -87,7 +88,7 @@ function SignIn() {
             </button>
           </div>
         </form>
-        {/* Google OAuth */}
+        <OAuth />
         <Link to="/sign-up" className="registerLink">
           Sign Up Instead
         </Link>
