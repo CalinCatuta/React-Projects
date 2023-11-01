f089514ab94c1b7e4fac8e1276ec671c71ac92de
CalinCatuta <calinut123@yahoo.co.uk>
2023-11-01 13:30:37 +0200

    add category and explore

--- house-finder/src/App.js
+++ house-finder/src/App.js
@@ -9,6 +9,7 @@ import PrivateRoute from "./components/PrivateRoute";
 import Explore from "./pages/Explore";
 import Offers from "./pages/Offers";
 import Profile from "./pages/Profile";
+import Category from "./pages/Category";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 import ForgotPassword from "./pages/ForgotPassword";
@@ -20,6 +21,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Explore />} />
           <Route path="/offers" element={<Offers />} />
+          <Route path="/category/:categoryName" element={<Category />} />
           <Route path="/profile" element={<PrivateRoute />}>
             <Route path="/profile" element={<Profile />} />
           </Route>
