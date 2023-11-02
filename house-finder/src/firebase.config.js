17d2311b5e3123799a99aff500f741dca7fd68a8
CalinCatuta <calinut123@yahoo.co.uk>
2023-11-02 19:32:51 +0200

    change firebase id

--- house-finder/src/firebase.config.js
+++ house-finder/src/firebase.config.js
@@ -1,16 +1,16 @@
-import { initializeApp } from 'firebase/app'
-import { getFirestore } from 'firebase/firestore'
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: 'AIzaSyDA8LVcBB6ZuFMGtZZLEh_veJ44WGrNRdE',
-  authDomain: 'house-marketplace-app-fb1d0.firebaseapp.com',
-  projectId: 'house-marketplace-app-fb1d0',
-  storageBucket: 'house-marketplace-app-fb1d0.appspot.com',
-  messagingSenderId: '832068369979',
-  appId: '1:832068369979:web:dce177da9bfc60a4b4e61e',
-}
+  apiKey: "AIzaSyDlwhCBFDHfMUhCkxU4GxJIojTESBfX0RI",
+  authDomain: "house-marketplace-app-8c52d.firebaseapp.com",
+  projectId: "house-marketplace-app-8c52d",
+  storageBucket: "house-marketplace-app-8c52d.appspot.com",
+  messagingSenderId: "941541211013",
+  appId: "1:941541211013:web:e03bd9c97e573716f4e2ec",
+};
 
 // Initialize Firebase
-initializeApp(firebaseConfig)
-export const db = getFirestore()
+initializeApp(firebaseConfig);
+export const db = getFirestore();
