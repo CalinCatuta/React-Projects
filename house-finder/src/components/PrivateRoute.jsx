5ecad554febd52fb6aaeea65e74f358b9e19f8ab
CalinCatuta <calinut123@yahoo.co.uk>
2023-11-02 19:22:19 +0200

    last-try

--- house-finder/src/components/PrivateRoute.jsx
+++ house-finder/src/components/PrivateRoute.jsx
@@ -1,14 +1,15 @@
-import { Navigate, Outlet } from "react-router-dom";
-import { useAuthStatus } from "../hooks/useAuthStatus";
-import Spinner from "./Spinner";
+import { Navigate, Outlet } from 'react-router-dom'
+import { useAuthStatus } from '../hooks/useAuthStatus'
+import Spinner from './Spinner'
 
 const PrivateRoute = () => {
-  const { loggedIn, loading } = useAuthStatus();
+  const { loggedIn, checkingStatus } = useAuthStatus()
 
-  if (loading) {
-    return <Spinner />;
+  if (checkingStatus) {
+    return <Spinner />
   }
-  return loggedIn ? <Outlet /> : <Navigate to="/sign-in" />;
-};
 
-export default PrivateRoute;
+  return loggedIn ? <Outlet /> : <Navigate to='/sign-in' />
+}
+
+export default PrivateRoute
--- house-finder/src/hooks/useAuthStatus.js
+++ house-finder/src/hooks/useAuthStatus.js
@@ -1,25 +1,32 @@
-import { useEffect, useState, useRef } from "react";
-import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { useEffect, useState, useRef } from 'react'
+import { getAuth, onAuthStateChanged } from 'firebase/auth'
 
 export const useAuthStatus = () => {
-  const [loggedIn, setLoggedIn] = useState(false);
-  const [loading, setLoading] = useState(true);
-  const isMounted = useRef(true);
-  // we use (useRef) to Fix memory leak warning
-  // stackoverflow(cleanup-memory-leaks-on-an-unmounted-component-in-react-hooks)
+  const [loggedIn, setLoggedIn] = useState(false)
+  const [checkingStatus, setCheckingStatus] = useState(true)
+  const isMounted = useRef(true)
+
   useEffect(() => {
     if (isMounted) {
-      const auth = getAuth();
+      const auth = getAuth()
       onAuthStateChanged(auth, (user) => {
         if (user) {
-          setLoggedIn(true);
+          setLoggedIn(true)
         }
-        setLoading(false);
-      });
+        setCheckingStatus(false)
+      })
     }
+
     return () => {
-      isMounted.current = false;
-    };
-  }, [isMounted]);
-  return { loggedIn, loading };
-};
+      isMounted.current = false
+    }
+  }, [isMounted])
+
+  return { loggedIn, checkingStatus }
+}
+
+// Protected routes in v6
+// https://stackoverflow.com/questions/65505665/protected-route-with-firebase
+
+// Fix memory leak warning
+// https://stackoverflow.com/questions/59780268/cleanup-memory-leaks-on-an-unmounted-component-in-react-hooks
