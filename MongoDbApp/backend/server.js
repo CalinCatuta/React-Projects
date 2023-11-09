dfdf9da8777f2d3a341c3164bb798f00cb80922b
CalinCatuta <calinut123@yahoo.co.uk>
2023-11-09 15:30:51 +0200

    error handler

--- MongoDbApp/backend/server.js
+++ MongoDbApp/backend/server.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
+const { errorHandlre } = require("../backend/middleware/errorMiddleware");
 const PORT = process.env.PORT || 5000;
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
   res.status(200).json({ message: "Welcome" });
@@ -10,5 +14,7 @@ app.get("/", (req, res) => {
 
 // Roytes
 app.use("/api/users", require("./routes/userRoutes"));
+
+app.use(errorHandlre);
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
